9a3365004a530721e3ea42b6443a352aada85a0a
BiancaIwata <bianca.silva@sptech.school>
2025-11-18 13:38:12 -0300

    finalizando modelagem e criando tabelas do banco

--- Banco de Dados/projeto_individual_script.sql
+++ Banco de Dados/projeto_individual_script.sql
@@ -7,3 +7,26 @@ nome VARCHAR(200) NOT NULL,
 email VARCHAR(100) NOT NULL UNIQUE,
 senha VARCHAR(50) NOT NULL
 );
+
+CREATE TABLE filmes (
+id_filme INT PRIMARY KEY AUTO_INCREMENT,
+nome VARCHAR(50),
+ano_lancamento YEAR
+);
+
+CREATE TABLE comentarios (
+id_comentario INT AUTO_INCREMENT,
+id_usuario INT,
+id_filme INT,
+PRIMARY KEY (id_comentario, id_usuario, id_filme),
+comentario TEXT,
+data_envio DATE,
+FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
+FOREIGN KEY (id_filme) REFERENCES filmes(id_filme)
+);
+
+CREATE TABLE emocoes (
+id_emocao INT PRIMARY KEY AUTO_INCREMENT,
+tipo VARCHAR(45),
+quantidade INT
+);
