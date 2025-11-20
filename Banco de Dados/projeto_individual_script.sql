27767ad95a41639ab12abda339f63fba0e87a6a3
BiancaIwata <bianca.silva@sptech.school>
2025-11-20 19:42:52 -0300

    atualizando banco de dados com nova regra de negócio

--- Banco de Dados/projeto_individual_script.sql
+++ Banco de Dados/projeto_individual_script.sql
@@ -14,6 +14,14 @@ nome VARCHAR(50),
 ano_lancamento YEAR
 );
 
+CREATE TABLE emocoes (
+id_emocao INT PRIMARY KEY AUTO_INCREMENT,
+tipo VARCHAR(45),
+intensidade VARCHAR(13)
+CONSTRAINT chkIntensidade
+	CHECK (intensidade IN ('Leve', 'Média', 'Intenso', 'Muito Intenso'))
+);
+
 CREATE TABLE comentarios (
 id_comentario INT AUTO_INCREMENT,
 id_usuario INT,
@@ -21,12 +29,9 @@ id_filme INT,
 PRIMARY KEY (id_comentario, id_usuario, id_filme),
 comentario TEXT,
 data_envio DATE,
+id_emocao INT,
 FOREIGN KEY (id_usuario) REFERENCES usuarios(id_usuario),
-FOREIGN KEY (id_filme) REFERENCES filmes(id_filme)
+FOREIGN KEY (id_filme) REFERENCES filmes(id_filme),
+FOREIGN KEY (id_emocao) REFERENCES emocoes(id_emocao)
 );
 
-CREATE TABLE emocoes (
-id_emocao INT PRIMARY KEY AUTO_INCREMENT,
-tipo VARCHAR(45),
-quantidade INT
-);
